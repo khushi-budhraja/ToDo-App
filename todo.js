30dc4d03d541ce046e6916080328e4fc48577cac
khushi-budhraja <kbudhraja21@gmail.com>
2024-01-06 10:31:57 +0530

    Can survive browser refresh.

--- todo.js
+++ todo.js
@@ -1,15 +1,16 @@
-todoList = [{item:'first', dueDate: '5/1/2024'},{item:'second', dueDate: '5/1/2024'}];   //list of objects(item, dueDate)
+todoList = [];   //array of objects(item, dueDate)
 displayItems();
 
 function addToDo(){
     let inputElement = document.querySelector('#input-text');
     let dateElement = document.querySelector('#todo-date');
-    todoList.push(
-                { item: inputElement.value, 
-                  dueDate: dateElement.value 
-                } );
-    inputElement.value = " ";
-    dateElement.value = " ";
+    let newObj = { item: inputElement.value, 
+                   dueDate: dateElement.value 
+                 }
+    todoList.push(newObj);
+    localStorage.setItem(`TodoItems`,JSON.stringify(todoList));
+    inputElement.value = "";
+    dateElement.value = "";
     displayItems();
 }
 
@@ -17,13 +18,16 @@ function displayItems(){
     let containerElement = document.querySelector(".todo-container");
     let newHtml = '';
 
+    todoListStr = localStorage.getItem('TodoItems');
+    todoList = todoListStr ? JSON.parse(todoListStr) : []
+
     for(let i=0; i<todoList.length; i++){
         //p and div are block elements so they print next element on next line. 
         //span is used so that button appear on same line.
         newHtml += `
             <span>${todoList[i].item}</span>   
             <span>${todoList[i].dueDate}</span>
-            <button class="del-button" onclick = "todoList.splice(${i},1);
+            <button class="del-button" onclick = "todoList.splice(${i},1); localStorage.setItem('TodoItems',JSON.stringify(todoList));
             displayItems();
             ">Delete</button> 
         `;
